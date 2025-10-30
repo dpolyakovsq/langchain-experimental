b06acf813e46f9bc15e8b60dfe6917f48cbd8342
tunforjob <tunforjob@gmail.com>
2025-10-30 14:28:23 +0200

    added langchain v1 support

--- libs/experimental/langchain_experimental/smart_llm/base.py
+++ libs/experimental/langchain_experimental/smart_llm/base.py
@@ -2,10 +2,10 @@
 
 from typing import Any, Dict, List, Optional, Tuple, Type
 
-from langchain.base_language import BaseLanguageModel
-from langchain.chains.base import Chain
-from langchain.input import get_colored_text
-from langchain.schema import LLMResult, PromptValue
+from langchain_classic.base_language import BaseLanguageModel
+from langchain_classic.chains.base import Chain
+from langchain_classic.input import get_colored_text
+from langchain_classic.schema import LLMResult, PromptValue
 from langchain_core.callbacks.manager import CallbackManagerForChainRun
 from langchain_core.prompts.base import BasePromptTemplate
 from langchain_core.prompts.chat import (
